1f8100f3e49992d9444a9cde23becaaf2841d3c5
Jacob Hell <jakehell95@gmail.com>
2016-02-06 06:15:32 -0500

    Added map, myo never locks now, updated events

--- public/javascripts/morse.js
+++ public/javascripts/morse.js
@@ -1,28 +1,58 @@
 Myo.connect('com.hackfsu.morse');
 
-Myo.on('status', function(data){
-	console.log("status");
+var map = {
+	a: ".-",
+	b: "-...",
+	c: "-.-.",
+	d: "-..",
+	e: ".",
+	f: "..-.",
+	g: "--.",
+	h: "....",
+	i: "..",
+	j: ".---",
+	k: "-.-",
+	l: ".-..",
+	m: "--",
+	n: "-.",
+	o: "---",
+	p: ".--.",
+	q: "--.-",
+	r: ".-.",
+	s: "...",
+	t: "-",
+	u: ".-.",
+	v: "...-",
+	w: ".--",
+	x: "-..-",
+	y: "-..-",
+	y: "-.--",
+	z: "--..",
+	space: "....."
+}
+
+Myo.on('connected', function(data){
+	Myo.setLockingPolicy("none");
 })
 
-
-//Whenever we get a pose event, we'll update the image sources with the active version of the image
+/*//Whenever we get a pose event, we'll update the image sources with the active version of the image
 Myo.on('pose', function(pose){
 	console.log("pose");
-})
+})*/
 
 //Opposite of above. We also revert the main img to the unlocked state
-Myo.on('pose_off', function(pose){
-	console.log("pose_off");
+Myo.on('fingers_spread', function(){
+	console.log("fingers off");
 });
 
 
 //Whenever a myo locks we'll switch the main image to a lock image
-Myo.on('locked', function(){
-	console.log("locked");
+Myo.on('fist', function(){
+	console.log("fist_off");
 });
 
 //Whenever a myo unlocks we'll switch the main image to a unlock image
-Myo.on('unlocked', function(){
-	console.log("hello");
+Myo.on('wave_in', function(){
+	console.log("wave");
 });
 
