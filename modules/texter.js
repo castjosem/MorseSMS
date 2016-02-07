58322a76d172a8081e3f76d8bd1668a15597da6f
Jacob Hell <jakehell95@gmail.com>
2016-02-07 05:50:23 -0500

    updating texter

--- modules/texter.js
+++ modules/texter.js
@@ -1,9 +1,31 @@
-var TMClient = require('textmagic-rest-client');
-  
-module.exports.texter = function(message){
+var accountSid = 'ACa51adcf30b56514ad5c1116a5edcbc34'; 
+var authToken = '657e472f5ca775e8777175d214e9cd91'; 
+ 
+//require the Twilio module and create a REST client 
+var client = require('twilio')(accountSid, authToken); 
 
-	var c = new TMClient('jacobhell', 'TMgoJDjFq2zOhkVdefIG6oMDeNDUAN');
-	c.Messages.send({text: message, phones:'13523623532'}, function(err, res){
-	    console.log('Messages.send()', err, res);
+module.exports.texter = function(message){
+	client.sms.messages.create({
+    	to:'+17863702278',
+	    from:'+17548885804',
+	    body: message
+	}, function(error, message) {
+		    // The HTTP request to Twilio will run asynchronously. This callback
+		    // function will be called when a response is received from Twilio
+		    // The "error" variable will contain error information, if any.
+		    // If the request was successful, this value will be "falsy"
+		    if (!error) {
+		        // The second argument to the callback will contain the information
+		        // sent back by Twilio for the request. In this case, it is the
+		        // information about the text messsage you just sent:
+		        console.log('Success! The SID for this SMS message is:');
+		        console.log(message.sid);
+		 
+		        console.log('Message sent on:');
+		        console.log(message.dateCreated);
+		    } else {
+		        console.log('Oops! There was an error.');
+		        console.log(error);
+		    }
 	});
 };
